4c3eb62ed6557709d7adf6cbe30ccb6883d3f507
iCode-Cat <devranboyac8@gmail.com>
2021-09-05 17:26:08 +0300

    profile update

--- src/api/profile.object.js
+++ src/api/profile.object.js
@@ -1,21 +1,22 @@
 module.exports.profile = [
-    {
-        image:'https://monoloop.com/wp-content/uploads/2017/08/photo-1511367461989-f85a21fda167.jpg',
-        name:'Dev Ran',
-        role:'Web developer',
-        about:'Self-motivated developer, who is willing to learn and create outstanding UI applications and helpful programs to the humanity.',
-        contact: [
-            {
-                label:'email',
-                text:'enyi970102@gmail.com',
-                icon:'email'
-            },
-            {   
-                label:'phone',
-                text:'(+603) 546 624 342',
-                icon:'call'
-            }
-        ]
-
-    }
-]
+  {
+    image:
+      'https://monoloop.com/wp-content/uploads/2017/08/photo-1511367461989-f85a21fda167.jpg',
+    name: 'Dev Ran',
+    role: 'Web developer',
+    about:
+      'Self-motivated developer, who is willing to learn and create outstanding UI applications and helpful programs to the humanity.',
+    contact: [
+      {
+        label: 'email',
+        text: 'marsmallusa@gmail.com',
+        icon: 'email',
+      },
+      {
+        label: 'phone',
+        text: '(+380) 63 987 0752',
+        icon: 'call',
+      },
+    ],
+  },
+];
